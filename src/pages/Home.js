3b2961c6fc41d8296f6da2a6d4d3b1d166eb488a
catalina5 <thanhbinh12369yt@gmail.com>
2023-08-17 14:51:30 +0700

    updade/api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,8 +14,8 @@ const Home = () => {
   const [responseBody, setResponseBody] = useState(null);
   useEffect(() => {
     const requestData = {
-      type: "link",
-      data: "https://momo-e-wallet.vercel.app/",
+      type: "text",
+      data: "03242342344",
       background: "rgb(255,255,255)",
       foreground: "rgb(0,0,0)",
       logo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.vjshop.vn%2Fmay-anh%2Fbai-viet-tin-tuc%2Fbest-camera-for-entry-level%2Fbest-camera-for-entry-level-1.jpeg&tbnid=ImFBJfaCmoYOQM&vet=12ahUKEwitnd_BmOOAAxWEvFYBHXNFBlAQMygCegQIARBa..i&imgrefurl=https%3A%2F%2Fvjshop.vn%2Ftin-tuc%2Fcong-nghe%2Ftop-camera-danh-cho-nhung-nguoi-moi-bat-dau&docid=BUj9SHKwMzG4gM&w=900&h=500&q=%E1%BA%A3nh%20m%C3%A1y&ved=2ahUKEwitnd_BmOOAAxWEvFYBHXNFBlAQMygCegQIARBa",
